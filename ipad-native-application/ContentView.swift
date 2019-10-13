41c156b8d5f22419020e333cda3f608034bf9901
Evgenii Khramkov <evkhramk@microsoft.com>
2019-10-12 23:24:13 -0700

    add macos support

---
+++ ipad-native-application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ipad-native-application
+//
+//  Created by Evgenii Khramkov on 10/12/19.
+//  Copyright © 2019 Evgenii Khramkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ipad-native-applicationTests/ipad_native_applicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  ipad_native_applicationTests.swift
+//  ipad-native-applicationTests
+//
+//  Created by Evgenii Khramkov on 10/12/19.
+//  Copyright © 2019 Evgenii Khramkov. All rights reserved.
+//
+
+import XCTest
+@testable import ipad_native_application
+
+class ipad_native_applicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
